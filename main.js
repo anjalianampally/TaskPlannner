6549e87a4d3d9c3e442ebd631f9d1e4336573ad9
anjalianampally <59692236+anjalianampally@users.noreply.github.com>
2020-03-03 21:32:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+/*   1602-17-737-320  */
+const addButton = document.querySelector('.addButton');
+var input = document.querySelector('.input');
+const container = document.querySelector('.container');
+class item{
+    constructor(itemName){
+        this.createDiv(itemName);
+    }
+createDiv(itemName){
+    let input = document.createElement('input');
+    input.value=itemName;
+    input.disabled=true;
+    input.classList.add('item_input');
+    input.type="text";
+    
+    let itemBox = document.createElement('div');
+    itemBox.classList.add('item');
+    
+    let removeButton = document.createElement('button');
+    removeButton.innerHTML="X";
+    removeButton.classList.add('removeButton');
+    container.appendChild(itemBox);
+    itemBox.appendChild(input);
+    itemBox.appendChild(removeButton);
+    removeButton.addEventListener('click',()=>this.remove(itemBox));
+}
+ remove(item){
+        container.removeChild(item);
+    }
+    
+}
+new item("sport");
+function check(){
+    if(input.value != ""){
+        new item(input.value);
+        input.value="";
+    }
+    
+}
+addButton.addEventListener('click',check);
+document.querySelector(".container").addEventListener('click',function(){
+document.querySelector(".item_input").style.background = "lightgreen";
+document.querySelector(".removeButton").style.background="lightgreen";
+})
+window.addEventListener('keydown', (e) =>{
+    if(e.which == 13){
+        check();
+    }
+})
